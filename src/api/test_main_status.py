2b8fcf67d9247949bef85053b43f208636cbde84
mdegrauwie <mdegrauw@outlook.fr>
2023-10-25 11:27:55 +0200

    Test main status API - hello world

--- src/api/test_main_status.py
+++ src/api/test_main_status.py
@@ -4,26 +4,7 @@ from main import app
 
 client = TestClient(app)
 
-data = {
-  "NAME_CONTRACT_TYPE": 0,
-  "CODE_GENDER": 1,
-  "FLAG_OWN_CAR": 0,
-  "FLAG_OWN_REALTY": 1,
-  "CNT_CHILDREN": 1,
-  "AMT_INCOME_TOTAL": 202500,
-  "AMT_CREDIT": 715500,
-  "AMT_ANNUITY": 21051,
-  "EXT_SOURCE_2": 0.13,
-  "EXT_SOURCE_3": 0.13,
-  "AGE": 32,
-  "ANCIENNITE": 4
-}
-
-#def test_main():
-#    response = client.post("/", json=data)
-#    assert response.status_code == 404
-
-def test_read_main():
+def test_main_status():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
