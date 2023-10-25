32ef2265a227d88fa364f69c9f6ad54a7bc93fea
mdegrauwie <mdegrauw@outlook.fr>
2023-10-25 09:19:40 +0200

    add test

---
+++ src/api/test_main_status.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient 
+
+from main api app 
+
+client = TestClient(app)
+
+data = {
+  "NAME_CONTRACT_TYPE": 0,
+  "CODE_GENDER": 0,
+  "FLAG_OWN_CAR": 0,
+  "FLAG_OWN_REALTY": 1,
+  "CNT_CHILDREN": 1,
+  "AMT_INCOME_TOTAL": 202500,
+  "AMT_CREDIT": 715500,
+  "AMT_ANNUITY": 21051,
+  "EXT_SOURCE_2": 0.13,
+  "EXT_SOURCE_3": 0.13,
+  "AGE": 33,
+  "ANCIENNITE": 4
+}
+
+def test_main():
+    response = client.post("/", json=data)
+    assert response.status_code == 200
