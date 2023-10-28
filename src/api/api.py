06df5724749fa8079028fd7fbf081a99c37751c6
mdegrauwie <mdegrauw@outlook.fr>
2023-10-28 13:34:48 +0200

    j

--- src/api/api.py
+++ src/api/api.py
@@ -11,10 +11,10 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from src.auth.auth_model import User, UserLoginSchema, UserInDB, Token
 
-from auth.auth_handler import users_db, verify_password, get_password_hash, get_user, authenticate_user, create_access_token, get_current_user
+from src.auth.auth_handler import users_db, verify_password, get_password_hash, get_user, authenticate_user, create_access_token, get_current_user
 
-from auth.auth_roles import RoleChecker
-from auth.auth_roles import get_user, get_current_user
+from src.auth.auth_roles import RoleChecker
+from src.auth.auth_roles import get_user, get_current_user
 
 
 # Bibliothèques pour connection à POSTGRES
