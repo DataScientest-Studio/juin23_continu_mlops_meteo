17421672f4515c96a92406b2dbe0a47e18f604d1
mdegrauwie <mdegrauw@outlook.fr>
2023-10-28 11:45:53 +0200

    Test CICD V3

--- src/api/api.py
+++ src/api/api.py
@@ -156,11 +156,6 @@ def get_prediction(data: data):
 
     return prediction[0]
 
-# added by MDG to test pytest
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
 
 
 nest_asyncio.apply()
