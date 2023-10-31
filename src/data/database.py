5e24f8582c14499403abf0fc96e7ddb7b9cf21f5
issambhd <issousam@gmail.com>
2023-10-31 17:50:04 +0100

    Add .env + Modify .gitignore

--- src/data/database.py
+++ src/data/database.py
@@ -7,7 +7,7 @@ from decouple import config
 
 DB_USERNAME = config("POSTGRES_USER")
 DB_PASSWORD = config("POSTGRES_PASSWORD")
-
+i
 #DB_SERVER = config("POSTGRES_SERVER")
 DB_SERVER = 'prj-network_from_compose'
 DB_PORT = config("POSTGRES_PORT")
@@ -15,8 +15,8 @@ DB_DATABASE = config("POSTGRES_DB")
 
 DB_URL = config("RDS_URL")
 
-#SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_DATABASE}"
-SQLALCHEMY_DATABASE_URL = DB_URL
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_DATABASE}"
+#SQLALCHEMY_DATABASE_URL = DB_URL
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
