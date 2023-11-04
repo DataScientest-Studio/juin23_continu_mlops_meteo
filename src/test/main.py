9a18c1ec59cbf26fd61c433c262e41bbef3bfb4e
mdegrauwie <mdegrauw@outlook.fr>
2023-11-04 13:07:59 +0100

    s,

--- src/test/main.py
+++ src/test/main.py
@@ -7,57 +7,7 @@ import io
 import pandas as pd
 import nest_asyncio
 
-
-import random
-import secrets
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pydantic import BaseModel, Field, EmailStr
-
-
 app = FastAPI()
-
-
-security = HTTPBasic()
-
-class user(BaseModel):
-  username : str 
-  password : str 
-
-def validate_credentials(credentials: HTTPBasicCredentials = Depends(security)):
-
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = b"pi"
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
-    current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = b"po"
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
-    if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
-
-
-@app.get("/users/me")
-def read_current_user(username: str = Depends(validate_credentials)):
-     return {"username": username}
-#     return username
-
-@app.post('/login')
-def get_username(user: user):
-    received = user.dict()
-    username = received['username']
-    password = received['password']
-    read_current_user(username)
-#    return {"username": username}
-    return username
 
 #Creating a class for the attributes input to the ML model.
 class data(BaseModel):
@@ -77,7 +27,7 @@ class data(BaseModel):
 
 
 #Loading the trained model
-with open("model.pkl", "rb") as f:
+with open("./model.pkl", "rb") as f:
     loaded_model = pickle.load(f)
     
 @app.post('/')
