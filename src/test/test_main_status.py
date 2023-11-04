dfc2ca81c7640b8842d0ab3b156db122a045d6d1
mdegrauwie <mdegrauw@outlook.fr>
2023-11-04 12:24:03 +0100

    Test MDG - CI/CD api_v2

---
+++ src/test/test_main_status.py
@@ -0,0 +1,16 @@
+#pytest 
+
+from fastapi.testclient import TestClient 
+
+# Added by Issam B. for pytest
+import sys  
+sys.path.append('src')
+
+from api_v2 import app 
+
+client = TestClient(app)
+
+def test_main_status():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "API 1.0.1 = OK (by Issam B.)"}
