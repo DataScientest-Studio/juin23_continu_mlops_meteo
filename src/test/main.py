601a07912f10430491a1aa9c2d36204cdd4367b0
mdegrauwie <mdegrauw@outlook.fr>
2023-11-04 13:15:07 +0100

    xx

--- src/test/main.py
+++ src/test/main.py
@@ -9,50 +9,9 @@ import nest_asyncio
 
 app = FastAPI()
 
-#Creating a class for the attributes input to the ML model.
-class data(BaseModel):
-	Location : float 
-	Rainfall : float 
-	WindGustDir : float
-	WindGustSpeed : float
-	WindDir9am : float
-	WindDir3am : float
-	WindSpeed9am : float
-	WindSpeed3am : float
-	Humidity9am : float
-	Humidity3am : float
-	Pressure3pm : float
-	Temp3pm: float
-	RainToday : float
-
-
-#Loading the trained model
-with open("./model.pkl", "rb") as f:
-    loaded_model = pickle.load(f)
-    
-@app.post('/')
-def get_prediction(data: data):
-    received = data.dict()
-    Location = received['Location']
-    Rainfall = received['Rainfall']
-    WindGustDir = received['WindGustDir']
-    WindGustSpeed = received['WindGustSpeed'] 
-    WindDir9am = received['WindDir9am']
-    WindDir3am = received['WindDir3am']
-    WindSpeed9am = received['WindSpeed9am']
-    WindSpeed3am = received['WindSpeed3am']
-    Humidity9am = received['Humidity9am']
-    Humidity3am = received['Humidity3am']
-    Pressure3pm = received['Pressure3pm']
-    Temp3pm = received['Temp3pm']
-    RainToday = received['RainToday']
-
-    prediction = loaded_model.predict_proba([[Location, Rainfall, WindGustDir, WindGustSpeed,
-                                        WindDir9am, WindDir3am, WindSpeed9am, WindSpeed3am,
-					Humidity9am, Humidity3am, Pressure3pm, Temp3pm, RainToday]])[:, 1]
-   
-
-    return prediction[0]
+@app.get("/")
+async def read_main():
+    return {"msg": "Test CI/CD - API status code"}
 
 nest_asyncio.apply()
 
